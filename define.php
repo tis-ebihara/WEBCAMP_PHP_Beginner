24d0a2635df389f8a2ffeaf2096ab74fd913c7a4
ebihra <ebihara@example.com>
2024-06-12 08:14:39 +0000

    define.phpを書いた

---
+++ define.php
@@ -0,0 +1,11 @@
+<?php
+
+// 定数の定義
+define('DEF_INT', 123); // 整数
+define('DEF_FLOAT', 3.14); // 少数
+define('DEF_STRING', "string"); // 文字列
+
+// 定数の使用
+var_dump(DEF_INT);
+var_dump(DEF_FLOAT);
+var_dump(DEF_STRING);
